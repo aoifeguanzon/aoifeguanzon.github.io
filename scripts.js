00fc2a96eabd0dd6ba20bf3ffec5d8f61a3f09ba
Aoife Guanzon <aoifeguanzon@gmail.com>
2025-05-04 18:56:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 const textSpan = document.getElementById('intro-text');
 const cursorSpan = document.getElementById('cursor');
-const nameSpan = document.getElementById('name'); 
+const nameSpan = document.getElementById('name');
 const desc = document.querySelector('.desc');
+const nav = document.querySelector('nav');
 
-const introText = "Hi, I'm ";
+const introText = "Hi, my name is ";
 const nameText = "Aoife";
 nameSpan.style.color = "#68172f";
 let index = 0;
@@ -14,15 +15,11 @@ function blinkCursor() {
 
 function typeWriter() {
   if (index < introText.length) {
-
     textSpan.textContent = introText.substring(0, index + 1);
     index++;
-    setTimeout(typeWriter, 30); 
+    setTimeout(typeWriter, 50);
   } else {
-
     cursorSpan.style.visibility = 'hidden';
-
-
     let nameIndex = 0;
     const nameTypingInterval = setInterval(() => {
       nameSpan.textContent = nameText.substring(0, nameIndex + 1);
@@ -32,20 +29,15 @@ function typeWriter() {
       if (nameIndex === nameText.length) {
         clearInterval(nameTypingInterval);
         cursorSpan.style.visibility = 'hidden';
-
         setTimeout(() => {
           desc.classList.add('show');
-        }, 200);
+          nav.classList.add('show');
+        }, 300);
       }
-    }, 30);
+    }, 40);
   }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   typeWriter();
-
-  const totalTypingTime = introText.length * 30 + nameText.length * 100;
-  setTimeout(() => {
-    document.querySelector('.desc').classList.add('show');
-  }, totalTypingTime + 100);
 });
