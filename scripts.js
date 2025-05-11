747107c4bf07a7a8dcd676d18d62568228c65354
Aoife Guanzon <aoifeguanzon@gmail.com>
2025-05-11 14:27:41 +0100

    No more cursor

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
 const textSpan = document.getElementById('intro-text');
-const cursorSpan = document.getElementById('cursor');
 const nameSpan = document.getElementById('name');
 const desc = document.querySelector('.desc');
 const nav = document.querySelector('nav');
 
 const introText = "Hi, my name is ";
-const nameText = "Aoife";
-nameSpan.style.color = "#68172f";
+const nameText = "Aoife 👋🏼";
+nameSpan.style.color = "#e6ffc8";
 let index = 0;
-
-function blinkCursor() {
-  cursorSpan.style.visibility = cursorSpan.style.visibility === 'hidden' ? 'visible' : 'hidden';
-}
 
 function typeWriter() {
   if (index < introText.length) {
@@ -19,16 +14,13 @@ function typeWriter() {
     index++;
     setTimeout(typeWriter, 50);
   } else {
-    cursorSpan.style.visibility = 'hidden';
     let nameIndex = 0;
     const nameTypingInterval = setInterval(() => {
       nameSpan.textContent = nameText.substring(0, nameIndex + 1);
       nameIndex++;
-      blinkCursor();
 
       if (nameIndex === nameText.length) {
         clearInterval(nameTypingInterval);
-        cursorSpan.style.visibility = 'hidden';
         setTimeout(() => {
           desc.classList.add('show');
           nav.classList.add('show');
